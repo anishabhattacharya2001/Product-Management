24d573b34deae544ae1b0904b6176291df609aab
Anisha <bhattacharyaanisha2001@gmail.com>
2022-06-28 10:06:43 -0400

    stars added

--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, Input } from '@angular/core';
 
 @Component({
   selector: 'app-star',
   templateUrl: './star.component.html',
   styleUrls: ['./star.component.css'],
 })
-export class StarComponent implements OnInit {
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
+export class StarComponent implements OnChanges {
+  @Input() rating = 5;
+  cropWidth = 75;
+  ngOnChanges(): void {
+    this.cropWidth = (this.rating * 75) / 5;
   }
 }
